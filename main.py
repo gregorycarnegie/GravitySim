669135eec0c6ea61eb190b442b73500dca260692
Gregory <36880726+gregorycarnegie@users.noreply.github.com>
2022-11-26 17:00:50 +0000

    minor changes

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-# This is a sample Python script.
 import numpy as np
-import time
 import pygame
 
 WHITE, YELLOW, BLUE, RED, DARK_GREY = (255, 255, 255), (255, 255, 0), (100, 149, 237), (188, 39, 50), (80, 78, 81)
-Grav, AU, TIME_STEP = 6.67408e-11, 1.495978707e11, 10 * 3600 * 12
+Grav, AU, TIME_STEP = 6.67408e-11, 1.495978707e11, 10 * 3600 * 12 / 10000
 SCALE = 30 / AU  # 1 AU = 100px
 
 
-def time_funk(func):
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        print(f'{func.__module__}, {func.__name__}: {time.perf_counter() - start} seconds')
-        return result
-
-    return wrapper
-
-
-@time_funk
 def accel(pos, mass, G, softening):
     dx, dy, dz = [position.T - position for position in [pos[:, i:i+1] for i in range(3)]]
     inv_r3 = (dx ** 2 + dy ** 2 + dz ** 2 + softening ** 2) ** (-1.5)
@@ -34,19 +21,17 @@ def nbody(n, trail):
     posgen, velgen, masgen = np.random.default_rng(233423), np.random.default_rng(456452), np.random.default_rng(553452)
 
     positions = posgen.standard_normal(size=(n, 3), dtype=np.float64) * AU * 100
-    vel = velgen.standard_normal(size=(n, 3), dtype=np.float64) * 1e8
+    vel = velgen.standard_normal(size=(n, 3), dtype=np.float64) * 5e7
     masses = np.absolute(masgen.standard_normal(size=(n, 1), dtype=np.float64)) * 2e32
 
-    positions[0] = vel[0] = np.zeros(shape=(1, 3))
+    positions[0] = vel[0] = np.zeros((1, 3))
     masses[0] = 2e39
 
-    dt = 10 * 3600 * 12
-    dt /= 10000
     run = True
     clock = pygame.time.Clock()
     t = 0
-
     clock.tick(120)
+
     if trail:
         WIN.fill((0, 0, 0))
 
@@ -54,27 +39,23 @@ def nbody(n, trail):
         if not trail:
             WIN.fill((0, 0, 0))
         acc = accel(positions, masses, Grav, 0.001)
-        i = 0
-        for planet in positions:
-            x, y = [int(w / AU) + (WIDTH * 0.5) for w in [planet[0], planet[1]]]
+        for i, planet in enumerate(positions):
+            x, y = planet[:2] / AU + WIDTH * 0.5
             if i == 0:
                 pygame.draw.circle(WIN, RED, (x, y), 10)
             else:
                 pygame.draw.circle(WIN, colours[i % len(colours)], (x, y), 1)
-            i += 1
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
-        positions += vel * dt + acc + dt * dt * 0.5
-        vel += acc * dt
-
-        t += dt
-
+        positions += vel * TIME_STEP + acc + TIME_STEP * TIME_STEP * 0.5
+        vel += acc * TIME_STEP
+        t += TIME_STEP
         pygame.display.update()
 
 
 if __name__ == '__main__':
     trail_line = True
-    nbody(32, trail_line)
+    nbody(128, trail_line)
